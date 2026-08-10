f62d6d673892f13eec9f47446a16e27c9fdc5688
belugaman9981 <littledage@outlook.com>
2026-08-10 16:39:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import re
+import regex
 from zhon import hanzi
 
-print(hanzi.characters)
+text = "测试 𠀀 123 hello"
+characters_found = regex.findall(r'\p{Han}', text)
 
-text = "这是一个测试，不对的话，请告诉我。"
-
-if re.match(f'^[{hanzi.characters}]+$', text):
-    print("Contains only Chinese characters!")
+print(characters_found) 
