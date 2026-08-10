db9a49fb233fea3ce64b6e006680cab9efa8e4d6
belugaman9981 <littledage@outlook.com>
2026-08-10 16:35:28 -0700

    :)

---
+++ test.py
@@ -0,0 +1,9 @@
+import re
+from zhon import hanzi
+
+print(hanzi.characters)
+
+text = "这是一个测试，不对的话，请告诉我。"
+
+if re.match(f'^[{hanzi.characters}]+$', text):
+    print("Contains only Chinese characters!")
